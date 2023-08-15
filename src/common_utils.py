d935940f830f99657a691fa0751b265dfb751229
andreitsev <bart-69@mail.ru>
2023-08-15 22:34:13 +0300

    couple intermediate commits

--- src/common_utils.py
+++ src/common_utils.py
@@ -1,3 +1,4 @@
+import itertools
 from datetime import datetime
 
 import pandas as pd
@@ -68,3 +69,110 @@ def clip_categories(
     mapping_dict = {**{k: other_category_name for k in vals_dict}, **{k: k for k in leave_these_values}}
     return  series.map(mapping_dict)
     
+    
+def choose_fighter_stats(raw_fights_df: pd.DataFrame, fighter_name: str) -> pd.DataFrame:
+    """Returns dataframe with fights stats for <fighter_name> fighter. 
+    This will be a subset of raw_fights_df dataframe (which necessary should have
+    names__1 and names__2 columns!)
+
+    Args:
+        raw_fights_df (pd.DataFrame): dataframe from which to make slice to make output
+        fighter_name (str): fighter name to make slice by
+
+    Returns:
+        one_fighter_stats_df: resulting dataframe with fights of a particular fighter
+    """
+    
+    assert 'names__1' in raw_fights_df.columns, 'names__1 column should be in raw_fights_df!'
+    assert 'names__2' in raw_fights_df.columns, 'names__2 column should be in raw_fights_df!'
+
+    one_fighter_stats_df = (
+        raw_fights_df.loc[
+            (raw_fights_df['names__1'] == fighter_name) 
+            | (raw_fights_df['names__2'] == fighter_name)
+        ]
+        .reset_index(drop=True)
+    )
+
+    # it's more convenient to always present chosen fighter in the first column
+    # but it's not always the case for raw_fights_df dataset, so we need to swap stats cols
+    
+    # making sure, that we only have pairs of columns or single columns (like 
+    # submission method, date, referee, etc.)
+    cols_dict = {}
+    for col in one_fighter_stats_df.columns:
+        if col[-1].isdigit():
+            col_name, fighter_1_or_2 = col[:-1], int(col[-1])
+        else:
+            col_name, fighter_1_or_2 = col, None
+        if col_name not in cols_dict:
+            cols_dict[col_name] = []
+        cols_dict[col_name].append(fighter_1_or_2)
+
+    columns_for_swapping = []
+    for col, values in cols_dict.items():
+        assert len(values) <= 2, f'len(values) should be <= 2, but got {len(values)} for {col}'
+        if len(values) == 2:
+            assert set(values) == set([1, 2]), \
+                f'set(values) should be set([1, 2]), but got {set(values)} for {col}'
+            columns_for_swapping.append(col)
+        elif len(values) == 1:
+             assert values[0] is None, \
+                f'values[0] should be None, but got {values[0]} for {col}'
+
+    # determining rows to swap columns
+    # those rows, where <fighter_name> fighter is not in names__1 column:
+    rows_to_swap = [
+        row[0] for row in one_fighter_stats_df[['names__1']].itertuples(index=True) 
+        if row[1] != fighter_name
+    ]
+
+    # here we're swapping columns:
+    for col in columns_for_swapping:
+        first = one_fighter_stats_df[f'{col}1'].tolist()
+        second = one_fighter_stats_df[f'{col}2'].tolist()
+        for idx in rows_to_swap:
+            first[idx], second[idx] = second[idx], first[idx]
+        one_fighter_stats_df[f'{col}1'] = first
+        one_fighter_stats_df[f'{col}2'] = second
+
+    return one_fighter_stats_df
+
+def get_ego_graph(
+    raw_fights_df: pd.DataFrame, 
+    fighter_name: str
+) -> pd.DataFrame:
+    """Returns ego-subgraph of a given fighter
+
+    Args:
+        raw_fights_df (pd.DataFrame): dataframe from which to make slice to make output
+        fighter_name (str): fighter name to make slice by
+    Returns:
+        pd.DataFrame: pd.DataFrame for ego subgraph
+    """
+    
+    res_df = (
+        raw_fights_df.loc[
+            (raw_fights_df['names__1'] == fighter_name) |
+            (raw_fights_df['names__2'] == fighter_name)
+        ]
+        .reset_index(drop=True)
+    )
+    hop1_fighters = set(
+        res_df['names__1'].tolist()
+        + res_df['names__2'].tolist()
+    )
+    for fighter in hop1_fighters:
+        other_fighter_df = (
+            raw_fights_df.loc[
+                (raw_fights_df['names__1'] == fighter) |
+                (raw_fights_df['names__2'] == fighter)
+            ]
+            .reset_index(drop=True)
+        )
+        res_df = pd.concat([
+            res_df,
+            other_fighter_df
+        ], axis=0)
+    res_df = res_df.drop_duplicates().reset_index(drop=True)
+    return res_df
