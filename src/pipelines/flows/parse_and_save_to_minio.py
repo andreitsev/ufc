3f8368ed502390414b35101f116b493540d61591
andreitsev <bart-69@mail.ru>
2024-03-17 13:02:43 +0300

    rewrote subflows as tasks and addef task_runner=SequentialTaskRunner() argument to flow decorator

--- src/pipelines/flows/parse_and_save_to_minio.py
+++ src/pipelines/flows/parse_and_save_to_minio.py
@@ -26,6 +26,8 @@ from minio import Minio
 from minio.api import Minio as MinioClient
 
 from prefect import task, flow
+from prefect.task_runners import SequentialTaskRunner
+
 
 from src.processing import eventslist2df
 from src.pipelines.parse_all_fights import parse_all_fights
@@ -63,57 +65,73 @@ def parse_cli():
         type=str,
         default='ufc_stats.json'
     )
+	parser.add_argument('--verbose', dest='verbose', default=False, action='store_true')
 	args = parser.parse_args()
 	return args
 
-def get_initialized_minio_client() -> MinioClient:
-    minio_client = initialize_minio_client(
+def get_initialized_minio_client(verbose: bool=False) -> MinioClient:
+	if verbose:
+		print("initialize_minio_client...")
+	minio_client = initialize_minio_client(
         ipaddr=minio_container_ipaddr(),
         access_key=MINIO_ACCESS_KEY,
         secret_key=MINIO_SECRET_KEY,
         port_number=9000
     )
-    return minio_client
+	return minio_client
 
-@flow(name='load_parsed_fights_from_minio',
-	  retries=3,
-	  retry_delay_seconds=10)
+@task(
+	name='load_parsed_fights_from_minio',
+	retries=3,
+	retry_delay_seconds=10,
+	log_prints=True,
+)
 def load_parsed_fights_from_minio(
     bucket_name: str, 
     object_name: str,
     minio_client: Optional[MinioClient]=None,
+	verbose: bool=False
 ) -> List[Dict[Any, Any]]:
-    all_fights_list = None
-    if minio_client is None:
-        minio_client = get_initialized_minio_client()
-
-    all_fights_list = load_json_from_minio(
+	all_fights_list = None
+	if minio_client is None:
+		minio_client = get_initialized_minio_client(verbose=verbose)
+	
+	if verbose: 
+		print("load_json_from_minio...")
+	all_fights_list = load_json_from_minio(
         minio_client=minio_client,
         bucket_name=bucket_name,
         object_name=object_name,
     )
-    return all_fights_list
+	return all_fights_list
 
 
-@flow(name='parsing_fights_not_in_events',
-	  retries=3,
-	  retry_delay_seconds=10)
+@task(
+	name='parsing_fights_not_in_events',
+	retries=3,
+	retry_delay_seconds=10,
+	log_prints=True
+)
 def parsing_fights_not_in_events(
 	save_path: Optional[str]=None,
     events_set: Optional[set]=None,
     all_fights_list: Optional[List[Dict[Any, Any]]]=None,
+	verbose: bool=False,
 ) -> List[Dict[Any, Any]]:
-    all_fights_list_added = parse_all_fights(
+	if verbose:
+		print('parse_all_fights...')
+	all_fights_list_added = parse_all_fights(
         save_path=save_path,
         parsed_events_set=events_set,
+		# parse_only_n_fights=4,
     )
-    if all_fights_list is None:
-        all_fights_list = all_fights_list_added
-    else:
-        all_fights_list.extend(all_fights_list_added)
-    return all_fights_list
+	if all_fights_list is None:
+		all_fights_list = all_fights_list_added
+	else:
+		all_fights_list.extend(all_fights_list_added)
+	return all_fights_list
 
-@flow(
+@task(
 	name='saving_fights_to_minio',
 	retries=3,
 	retry_delay_seconds=10,
@@ -124,53 +142,66 @@ def saving_fights_to_minio(
     bucket_name: str,
     object_name: str,
     minio_client: Optional[MinioClient]=None,
+	verbose: bool=False,
 ) -> None:
-    
-    if minio_client is None:
-        minio_client = initialize_minio_client(
-            ipaddr=minio_container_ipaddr(),
-            access_key=MINIO_ACCESS_KEY,
-            secret_key=MINIO_SECRET_KEY,
-            port_number=9000
-        )
+	
+	if minio_client is None:
+		minio_client = get_initialized_minio_client(verbose=verbose)
 
-    save_json_to_minio(
+	if verbose:
+		print("save_json_to_minio...")
+	save_json_to_minio(
         obj=all_fights_list,
         minio_client=minio_client,
         bucket_name=bucket_name,
         object_name=object_name,
     )
-    return
+	return
 
 @flow(
 	name='main_flow',
 	retries=3,
 	retry_delay_seconds=10,
-	log_prints=True
+	log_prints=True,
+	task_runner=SequentialTaskRunner()
 )
 def main_flow(
 	save_path: str,
 	minio_bucket_name: str,
 	minio_object_name: str,
+	verbose: bool=False,
 ) -> None:
-	minio_client = get_initialized_minio_client()
+	if verbose:
+		print("get_initialized_minio_client...")
+	minio_client = get_initialized_minio_client(verbose=verbose)
+	if verbose:
+		print("load_parsed_fights_from_minio...")
 	parsed_events_from_minio = load_parsed_fights_from_minio(
 		bucket_name=minio_bucket_name, 
 		object_name=minio_object_name,
 		minio_client=minio_client,
+		verbose=verbose
 	)
 	events_set = set([fight_info['event_uri'] for fight_info in parsed_events_from_minio])
+	if verbose:
+		print("parsing_fights_not_in_events...")
 	all_fights_list = parsing_fights_not_in_events(
 		save_path=save_path,
 		events_set=events_set,
     	all_fights_list=parsed_events_from_minio,
+		verbose=verbose
 	)
+	if verbose:
+		print("saving_fights_to_minio...")
 	saving_fights_to_minio(
 		all_fights_list=all_fights_list,
 		bucket_name=minio_bucket_name,
     	object_name=minio_object_name,
 		minio_client=minio_client,
+		verbose=verbose
 	)
+	if verbose:
+		print("OK!")
 	return
 
 
@@ -205,6 +236,7 @@ if __name__ == '__main__':
 		save_path=args.save_path,
 		minio_bucket_name=args.minio_bucket_name,
 		minio_object_name=args.minio_object_name,
+		verbose=args.verbose,
 	)
 	end = time.perf_counter()
 	logging.info(f'all fights parsed for {(end - st) // 60} minutes {round((end - st) % 60)} seconds')
